0a8eb84238430d2f1991575d029e7d2ff713982c
K D <diego.unipampa@gmail.com>
2023-10-02 17:15:25 -0300

    updates

--- run_demo_docker.sh
+++ run_demo_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DIR=$(readlink -f shared)
+DIR=$(readlink -f files)
 DOCKER_IMAGE=sf23/pdfwebsigner:latest
 sudo docker inspect $DOCKER_IMAGE &> /dev/null
 if [ $? -ne 0 ]
 then
     sudo docker image pull $DOCKER_IMAGE
 fi
-sudo docker run -it --name=pdfwebsigner-$RANDOM -p 127.0.0.1:8888:8888 -v $DIR:/var/www/html/shared -e DISPLAY=unix$DISPLAY $DOCKER_IMAGE
+sudo docker run -it --name=pdfwebsigner-$RANDOM -p 127.0.0.1:8888:8888 -v $DIR:/var/www/html/files -e DISPLAY=unix$DISPLAY $DOCKER_IMAGE
