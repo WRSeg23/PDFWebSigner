37cad5710ceefff31061a2dabd6aa9b18caf0c7a
K D <diego.unipampa@gmail.com>
2023-10-03 18:06:10 -0300

    updates

--- pdfwebsigner.sh
+++ pdfwebsigner.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-sudo docker images 
-
+echo "========================================================"
+echo -n "Inicializando o container Docker ... "
 ./run_demo_docker.sh &> logs/demo_docker.log &  
+sleep 3
+echo "done."
+echo "========================================================"
 
-sleep 5
 
+echo "========================================================"
+echo -n "Inicializando o navegador ... "
 ./run_demo_browser.sh &> logs/demo_browser.log & 
+sleep 1
+echo "done."
+echo "========================================================"
+
+echo "========================================================"
+echo "Monitorando os logs de execução do container Docker ... "
+echo "          <precione Ctrl+C para finalizar>"
+echo "========================================================"
 
 tail -f logs/demo_docker.log 
